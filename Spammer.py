b489423abbbcc3442277a581b26dc1f2c7d2a092
Jay <63338983+NirmithVictor@users.noreply.github.com>
2020-06-16 14:36:10 -0400

    Update Spammer.py

--- Spammer.py
+++ Spammer.py
@@ -2,6 +2,15 @@ import pyautogui
 import time
 a=input("Enter Your Message:")
 b=int(input("Enter Number Of Times You Wanna Spam:"))
-time.sleep(5)
+
+
+Timecounter=5
+while(Timecounter!=0):
+    time.sleep(1)
+    print(Timecounter)
+    Timecounter=Timecounter-1
+time.sleep(1.5)
+print("Sayanora Motherfucker")
+
 for i in range(b):
     pyautogui.typewrite(a+'\n')
